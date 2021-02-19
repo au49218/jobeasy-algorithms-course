ae812f73f4b1a8cc24987623d7d939dc5aeaf049
au49218 <bucksbin2k1@yahoo.com>
2021-02-18 21:20:18 -0600

    Algorithms Lesson 1 Homework 3

--- algorithms/lesson_1/algorithms_lesson_1_hw_3.py
+++ algorithms/lesson_1/algorithms_lesson_1_hw_3.py
@@ -1,6 +1,26 @@
 # Count odd and even digits of the whole number.
 # If entered number 34560, then 3 digits will be even (4, 6, and 0) and 2 odd digits (3 and 5).
 
+def count_even_odd(number):
+    count_even = 0
+    count_odd = 0
+
+    while number > 0:
+        remainder = number % 10
+        if remainder % 2 == 0:
+            count_even = count_even + 1
+        else:
+            count_odd = count_odd + 1
+        number //= 10
+
+    print("Single - Even numbers: ", count_even)
+    print("Single - Odd numbers: ", count_odd)
+
+
+count_even_odd(34560)
+count_even_odd(77079)
+
+
 def even_odd(number_list):
     count_even = 0
     count_odd = 0
@@ -13,13 +33,12 @@ def even_odd(number_list):
         else:
             count_odd = count_odd + 1
 
-    print("Even numbers: ", count_even)
-    print("Odd numbers: ", count_odd)
+    print("Multiple - Even numbers: ", count_even)
+    print("Multiple - Odd numbers: ", count_odd)
 
 
 number_list_array = [71, 23, 16, 47, 68, 95, 3, 5, 34, 44]
-even_odd(number_list_array)
+# even_odd(number_list_array)
 
 number_list_array = [22, 23, 18, 44, 72, 92, 6, 3, 31, 75]
-even_odd(number_list_array)
-
+# even_odd(number_list_array)
