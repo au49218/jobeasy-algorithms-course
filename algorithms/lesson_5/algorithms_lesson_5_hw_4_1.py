9fd311f4ed5cc8ac69f566323ffd51f92a4248cd
au49218 <bucksbin2k1@yahoo.com>
2021-03-26 18:54:14 -0500

    Algorithms Lesson 5 Homework

---
+++ algorithms/lesson_5/algorithms_lesson_5_hw_4_1.py
@@ -0,0 +1,22 @@
+# Codewars: Multiples of 3 or 5
+#
+# If we list all the natural numbers below 10 that are multiples
+# of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is
+# 23. Finish the solution so that it returns the sum of all the
+# multiples of 3 or 5 below the number passed in. Note: If the
+# number is a multiple of both 3 and 5, only count it once. Also,
+# if a number is negative, return 0.
+
+
+def multiples(limit):
+
+    x = 0
+    for i in range(1, limit):
+        if not i % 5 or not i % 3:
+            x = x + i
+    print(x)
+
+
+multiples(10)
+multiples(45)
+
